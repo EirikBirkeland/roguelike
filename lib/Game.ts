f1ae6c9fc80047292b297f5bad9db07e479be0d0
eirikbirkeland <birketrans@gmail.com>
2020-05-26 01:51:52 +0200

    Add placeholder monster attack

--- lib/Game.ts
+++ lib/Game.ts
@@ -1,6 +1,6 @@
+import { Player } from './animate/beings';
 import GFX from './constants';
-
-const invalidDestinationTiles = [GFX.WALL, GFX.VOID, GFX.ENEMY];
+const invalidDestinationTiles = [GFX.WALL, GFX.VOID];
 
 // We probably don't need to instantiate the game
 export default class Game {
@@ -18,15 +18,23 @@ export default class Game {
         // Huge problem: This single-layer symbol-based grid does not support multiple items existing in a single tile.
         // Maybe I can use an AoA in each tile?
         this.registeredObjects.forEach((obj) => {
+
             // if the obj's X and Y is not on top of a wall or void tile...
             if (!invalidDestinationTiles.includes(myGrid[obj['y']][obj['x']])) {
-                myGrid[obj['y']][obj['x']] = obj.graphic;
+
+                if(obj instanceof Player && myGrid[obj['y']][obj['x']] === GFX.ENEMY) {
+                    // fight the enemy!
+                    console.log("You attack the nasty enemy");
+                } else {
+                    myGrid[obj['y']][obj['x']] = obj.graphic;
+                }
             } else {
             // else restore previous coords
                 obj.x = obj.prevX;
                 obj.y = obj.prevY;
                 myGrid[obj['y']][obj['x']] = obj.graphic;
             }
+
         });
 
         return myGrid.map(line => line.join('')).join('\n');
